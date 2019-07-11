0b304ca3783793c14c6c5208a0790e59664e321d
Brian Major <major.brian@gmail.com>
2019-07-11 15:45:57 -0700

    GroupClientImpl for GMSClient

---
+++ cadc-access-control/src/main/java/ca/nrc/cadc/gms/GroupClientImpl.java
@@ -0,0 +1,127 @@
+/*
+ ************************************************************************
+ *******************  CANADIAN ASTRONOMY DATA CENTRE  *******************
+ **************  CENTRE CANADIEN DE DONNÉES ASTRONOMIQUES  **************
+ *
+ *  (c) 2019.                            (c) 2019.
+ *  Government of Canada                 Gouvernement du Canada
+ *  National Research Council            Conseil national de recherches
+ *  Ottawa, Canada, K1A 0R6              Ottawa, Canada, K1A 0R6
+ *  All rights reserved                  Tous droits réservés
+ *
+ *  NRC disclaims any warranties,        Le CNRC dénie toute garantie
+ *  expressed, implied, or               énoncée, implicite ou légale,
+ *  statutory, of any kind with          de quelque nature que ce
+ *  respect to the software,             soit, concernant le logiciel,
+ *  including without limitation         y compris sans restriction
+ *  any warranty of merchantability      toute garantie de valeur
+ *  or fitness for a particular          marchande ou de pertinence
+ *  purpose. NRC shall not be            pour un usage particulier.
+ *  liable in any event for any          Le CNRC ne pourra en aucun cas
+ *  damages, whether direct or           être tenu responsable de tout
+ *  indirect, special or general,        dommage, direct ou indirect,
+ *  consequential or incidental,         particulier ou général,
+ *  arising from the use of the          accessoire ou fortuit, résultant
+ *  software.  Neither the name          de l'utilisation du logiciel. Ni
+ *  of the National Research             le nom du Conseil National de
+ *  Council of Canada nor the            Recherches du Canada ni les noms
+ *  names of its contributors may        de ses  participants ne peuvent
+ *  be used to endorse or promote        être utilisés pour approuver ou
+ *  products derived from this           promouvoir les produits dérivés
+ *  software without specific prior      de ce logiciel sans autorisation
+ *  written permission.                  préalable et particulière
+ *                                       par écrit.
+ *
+ *  This file is part of the             Ce fichier fait partie du projet
+ *  OpenCADC project.                    OpenCADC.
+ *
+ *  OpenCADC is free software:           OpenCADC est un logiciel libre ;
+ *  you can redistribute it and/or       vous pouvez le redistribuer ou le
+ *  modify it under the terms of         modifier suivant les termes de
+ *  the GNU Affero General Public        la “GNU Affero General Public
+ *  License as published by the          License” telle que publiée
+ *  Free Software Foundation,            par la Free Software Foundation
+ *  either version 3 of the              : soit la version 3 de cette
+ *  License, or (at your option)         licence, soit (à votre gré)
+ *  any later version.                   toute version ultérieure.
+ *
+ *  OpenCADC is distributed in the       OpenCADC est distribué
+ *  hope that it will be useful,         dans l’espoir qu’il vous
+ *  but WITHOUT ANY WARRANTY;            sera utile, mais SANS AUCUNE
+ *  without even the implied             GARANTIE : sans même la garantie
+ *  warranty of MERCHANTABILITY          implicite de COMMERCIALISABILITÉ
+ *  or FITNESS FOR A PARTICULAR          ni d’ADÉQUATION À UN OBJECTIF
+ *  PURPOSE.  See the GNU Affero         PARTICULIER. Consultez la Licence
+ *  General Public License for           Générale Publique GNU Affero
+ *  more details.                        pour plus de détails.
+ *
+ *  You should have received             Vous devriez avoir reçu une
+ *  a copy of the GNU Affero             copie de la Licence Générale
+ *  General Public License along         Publique GNU Affero avec
+ *  with OpenCADC.  If not, see          OpenCADC ; si ce n’est
+ *  <http://www.gnu.org/licenses/>.      pas le cas, consultez :
+ *                                       <http://www.gnu.org/licenses/>.
+ *
+ *  $Revision: 4 $
+ *
+ ************************************************************************
+ */
+
+package ca.nrc.cadc.gms;
+
+import java.net.URI;
+import java.security.AccessControlException;
+import java.security.cert.CertificateExpiredException;
+import java.security.cert.CertificateNotYetValidException;
+import java.util.List;
+
+import org.apache.log4j.Logger;
+
+import ca.nrc.cadc.ac.client.GMSClient;
+import ca.nrc.cadc.cred.client.CredUtil;
+
+/**
+ * This class is acting as a bridge between the GroupClient in cadc-gms
+ * and the cadc-access-control implementation named GMSClient.
+ * 
+ * In cadc-gms, GroupUtil, the group client is loaded by trying to load
+ * a class with the name ca.nrc.cadc.gms.GroupClientImpl.
+ * 
+ * This class will be removed when GMSClient is renamed to be the name
+ * of this class.
+ * 
+ * @author majorb
+ *
+ */
+public class GroupClientImpl implements GroupClient {
+    
+    Logger log = Logger.getLogger(GroupClientImpl.class);
+    
+    private GMSClient gmsClient;
+    
+    public GroupClientImpl(URI serviceID) {
+        this.gmsClient = new GMSClient(serviceID);
+    }
+
+    @Override
+    public List<GroupURI> getMemberships() {
+        checkCredentials();
+        return gmsClient.getMemberships();
+    }
+
+    @Override
+    public boolean isMember(GroupURI groupURI) {
+        checkCredentials();
+        return gmsClient.isMember(groupURI);
+    }
+    
+    private void checkCredentials() {
+        try {
+            CredUtil.checkCredentials();
+        } catch (Exception e) {
+            log.error("Failed to check credentials for group call", e);
+            throw new AccessControlException(e.getMessage());
+        }
+    }
+
+}
