603c5196e8aa9ee84223014824156e7348c041e7
Patrick Dowler <pdowler.cadc@gmail.com>
2023-06-15 10:59:02 -0700

    fix javadoc for java 11 build

--- cadc-access-control/src/main/java/ca/nrc/cadc/ac/ReaderException.java
+++ cadc-access-control/src/main/java/ca/nrc/cadc/ac/ReaderException.java
@@ -66,6 +66,7 @@
  *
  ************************************************************************
  */
+
 package ca.nrc.cadc.ac;
 
 /**
@@ -88,14 +89,14 @@ public class ReaderException extends Exception
 
     /**
      * Constructs a new exception with the specified detail message and
-     * cause.  <p>Note that the detail message associated with
+     * cause. Note that the detail message associated with
      * <code>cause</code> is <i>not</i> automatically incorporated in
      * this exception's detail message.
      *
      * @param message the detail message (which is saved for later retrieval
      *                by the {@link #getMessage()} method).
      * @param cause   the cause (which is saved for later retrieval by the
-     *                {@link #getCause()} method).  (A <tt>null</tt> value is
+     *                {@link #getCause()} method).  (A null value is
      *                permitted, and indicates that the cause is nonexistent or
      *                unknown.)
      * @since 1.4
