6a38e0865a8523dec10228207286a84d5767da4c
Jeff Burke <jburke.cadc@gmail.com>
2023-09-25 08:38:10 -0700

    CADC-12766 first draft of PosixMapperClient implementation

--- cadc-gms/src/main/java/org/opencadc/auth/PosixMapperClient.java
+++ cadc-gms/src/main/java/org/opencadc/auth/PosixMapperClient.java
@@ -67,10 +67,28 @@
 
 package org.opencadc.auth;
 
+import ca.nrc.cadc.auth.AuthMethod;
+import ca.nrc.cadc.auth.AuthenticationUtil;
+import ca.nrc.cadc.auth.HttpPrincipal;
 import ca.nrc.cadc.auth.PosixPrincipal;
+import ca.nrc.cadc.net.HttpGet;
+import ca.nrc.cadc.net.ResourceAlreadyExistsException;
+import ca.nrc.cadc.net.ResourceNotFoundException;
+import ca.nrc.cadc.reg.Capabilities;
+import ca.nrc.cadc.reg.Standards;
+import ca.nrc.cadc.reg.client.RegistryClient;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.net.URI;
+import java.net.URL;
+import java.security.Principal;
+import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.Iterator;
 import java.util.List;
+import java.util.Set;
 import javax.security.auth.Subject;
 import org.apache.log4j.Logger;
 import org.opencadc.gms.GroupURI;
@@ -84,29 +102,205 @@ public class PosixMapperClient {
     private static final Logger log = Logger.getLogger(PosixMapperClient.class);
 
     private final URI resourceID;
+
+    private final RegistryClient regClient = new RegistryClient();
     
-    public PosixMapperClient(URI resourceID) {
+    public PosixMapperClient(URI resourceID) throws IOException, ResourceNotFoundException {
         this.resourceID = resourceID;
+        if (resourceID == null) {
+            throw new IllegalArgumentException("resourceID cannot be null");
+        }
+
+        Capabilities caps = regClient.getCapabilities(resourceID);
+        if (caps == null) {
+            throw new ResourceNotFoundException("service not found in registry: " + resourceID);
+        }
     }
     
     // use case: cavern needs PosixPrincipal added to the caller subject for create node
     // use case: cavern needs to recreate Subject from PosixPrincipal to output node
     // detail: StandardIdentityManager calls this to add local posix identity for caller
     // proposal: add defaultGroup to PosixPrincipal, would be returned here
-    public Subject augment(Subject s) {
-        throw new UnsupportedOperationException();
+    public Subject augment(Subject subject)
+        throws ResourceNotFoundException, IOException, InterruptedException {
+        if (subject == null) {
+            throw new IllegalArgumentException("subject cannot be null");
+        }
+
+        AuthMethod authMethod = AuthenticationUtil.getAuthMethodFromCredentials(subject);
+        if (authMethod == null || AuthMethod.ANON.equals(authMethod)) {
+            throw new UnsupportedOperationException("cannot augment subject for an anonymous user");
+        }
+
+        URL serviceURL = regClient.getServiceURL(resourceID, Standards.POSIX_USERMAP, authMethod);
+        if (serviceURL == null) {
+            throw new ResourceNotFoundException("service not found for " + resourceID);
+        }
+
+        Set<PosixPrincipal> posixPrincipals = subject.getPrincipals(PosixPrincipal.class);
+        Set<HttpPrincipal> httpPrincipals = subject.getPrincipals(HttpPrincipal.class);
+        if (!posixPrincipals.isEmpty() && !httpPrincipals.isEmpty()) {
+            return subject; //nothing to do ?????
+        }
+
+        if (posixPrincipals.isEmpty() && httpPrincipals.isEmpty()) {
+            throw new IllegalArgumentException("Subject must contain either a HttpPrincipal or a PosixPrincipal");
+        }
+
+        String user = null;
+        Integer uid = null;
+        if (!posixPrincipals.isEmpty()) {
+            uid = posixPrincipals.iterator().next().getUidNumber();
+        } else {
+            user = httpPrincipals.iterator().next().getName();
+        }
+
+        StringBuilder query = new StringBuilder(serviceURL.toExternalForm());
+        query.append("?");
+        if (user != null) {
+            query.append("user=").append(user);
+        } else {
+            query.append("uid=").append(uid);
+        }
+        URL queryURL = new URL(query.toString());
+
+        try {
+            HttpGet get = new HttpGet(queryURL, true);
+            get.setRequestProperty("accept", "text/tab-separated-values");
+            get.prepare();
+
+            InputStream results = get.getInputStream();
+            InputStreamReader r = new InputStreamReader(results);
+            try (BufferedReader reader = new BufferedReader(new InputStreamReader(results))) {
+                String line = reader.readLine();
+                String[] tokens = line.split("\\s");
+                if (tokens.length != 3) {
+                    throw new IllegalStateException(
+                            String.format("error parsing query results, expected 3 values, found %s: %s",
+                                    tokens.length, line));
+                }
+                // format - username uid gid
+                Set<Principal> principals = new HashSet<>();
+                principals.add(new HttpPrincipal(tokens[0]));
+                principals.add(new PosixPrincipal(Integer.parseInt(tokens[1])));
+                for (Principal p : subject.getPrincipals()) {
+                    if (!(p instanceof HttpPrincipal) && !(p instanceof PosixPrincipal)) {
+                        principals.add(p);
+                    }
+                }
+                return new Subject(false, principals, subject.getPublicCredentials(), subject.getPrivateCredentials());
+            }
+        } catch (ResourceAlreadyExistsException e) {
+            throw new RuntimeException("BUG: unexpected failure: " + e, e);
+        }
     }
     
     // use case: cavern uses this when caller tries to set group permissions
     // use case: skaha uses this to generate securityContext for a user container
     // detail: this may create and persist a local GID as a side effect
-    public List<PosixGroup> getGID(List<GroupURI> groups) {
-        throw new UnsupportedOperationException();
+    public List<PosixGroup> getGID(List<GroupURI> groups)
+            throws ResourceNotFoundException, IOException, InterruptedException {
+        if (groups == null) {
+            throw new IllegalArgumentException("groups cannot be null");
+        }
+
+        Subject subject = AuthenticationUtil.getCurrentSubject();
+        AuthMethod authMethod = AuthenticationUtil.getAuthMethodFromCredentials(subject);
+        if (authMethod == null || AuthMethod.ANON.equals(authMethod)) {
+            throw new UnsupportedOperationException("cannot augment subject for anonymous");
+        }
+
+        URL serviceURL = regClient.getServiceURL(resourceID, Standards.POSIX_GROUPMAP, authMethod);
+        if (serviceURL == null) {
+            throw new ResourceNotFoundException("not found: " + resourceID);
+        }
+
+        StringBuilder query = new StringBuilder(serviceURL.toExternalForm());
+        String sep = "?";
+        for (GroupURI groupURI : groups) {
+            query.append(sep).append("group=").append(groupURI.getURI().toASCIIString());
+            sep = "&";
+        }
+        URL queryURL = new URL(query.toString());
+
+        try {
+            HttpGet get = new HttpGet(queryURL, true);
+            get.setRequestProperty("accept", "text/tab-separated-values");
+            get.prepare();
+
+            List<PosixGroup> posixGroups = new ArrayList<>();
+            InputStream results = get.getInputStream();
+            try (BufferedReader reader = new BufferedReader(new InputStreamReader(results))) {
+                while (reader.ready()) {
+                    String line = reader.readLine();
+                    String[] tokens = line.split("\\s+");
+                    if (tokens.length != 2) {
+                        throw new IllegalStateException(
+                                String.format("error parsing query results, expected 2 values, found %s: %s",
+                                        tokens.length, line));
+                    }
+                    GroupURI groupURI = new GroupURI(URI.create(tokens[0]));
+                    Integer gid = Integer.getInteger(tokens[1]);
+                    posixGroups.add(new PosixGroup(gid, groupURI));
+                }
+            }
+            return posixGroups;
+        } catch (ResourceAlreadyExistsException e) {
+            throw new RuntimeException("BUG: unexpected failure: " + e, e);
+        }
     }
     
     // use case: cavern uses this when reading a node from disk and output the node doc
-    public List<PosixGroup> getURI(List<Integer> groups) {
-        throw new UnsupportedOperationException();
+    public List<PosixGroup> getURI(List<Integer> groups)
+            throws ResourceNotFoundException, IOException, InterruptedException {
+        if (groups == null) {
+            throw new IllegalArgumentException("groups cannot be null");
+        }
+
+        Subject subject = AuthenticationUtil.getCurrentSubject();
+        AuthMethod authMethod = AuthenticationUtil.getAuthMethodFromCredentials(subject);
+        if (authMethod == null || AuthMethod.ANON.equals(authMethod)) {
+            throw new UnsupportedOperationException("cannot augment subject for anonymous");
+        }
+
+        URL serviceURL = regClient.getServiceURL(resourceID, Standards.POSIX_GROUPMAP, authMethod);
+        if (serviceURL == null) {
+            throw new ResourceNotFoundException("not found: " + resourceID);
+        }
+
+        StringBuilder query = new StringBuilder(serviceURL.toExternalForm());
+        String sep = "?";
+        for (Integer gid : groups) {
+            query.append(sep).append("gid=").append(gid);
+            sep = "&";
+        }
+        URL queryURL = new URL(query.toString());
+
+        try {
+            HttpGet get = new HttpGet(queryURL, true);
+            get.setRequestProperty("accept", "text/tab-separated-values");
+            get.prepare();
+
+            List<PosixGroup> posixGroups = new ArrayList<>();
+            InputStream results = get.getInputStream();
+            try (BufferedReader reader = new BufferedReader(new InputStreamReader(results))) {
+                while (reader.ready()) {
+                    String line = reader.readLine();
+                    String[] tokens = line.split("\\s+");
+                    if (tokens.length != 2) {
+                        throw new IllegalStateException(
+                                String.format("error parsing query results, expected 2 values, found %s: %s",
+                                        tokens.length, line));
+                    }
+                    Integer gid = Integer.getInteger(tokens[0]);
+                    GroupURI groupURI = new GroupURI(URI.create(tokens[1]));
+                    posixGroups.add(new PosixGroup(gid, groupURI));
+                }
+            }
+            return posixGroups;
+        } catch (ResourceAlreadyExistsException e) {
+            throw new RuntimeException("BUG: unexpected failure: " + e, e);
+        }
     }
     
     // use case: skaha needs the complete username-uid map for user containers
