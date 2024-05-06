112523a191ea3736d4b9a500f63ba5997d47eef5
Dustin Jenkins <djenkins.cadc@gmail.com>
2024-05-06 12:16:56 -0700

    Added logging.

--- posix-mapper/src/main/java/org/opencadc/posix/mapper/web/PosixMapperListener.java
+++ posix-mapper/src/main/java/org/opencadc/posix/mapper/web/PosixMapperListener.java
@@ -2,6 +2,7 @@ package org.opencadc.posix.mapper.web;
 
 import ca.nrc.cadc.auth.AuthenticationUtil;
 import ca.nrc.cadc.auth.IdentityManager;
+import org.apache.log4j.Logger;
 import org.opencadc.posix.mapper.auth.DelegatingAPIKeyIdentityManager;
 
 import javax.servlet.ServletContextEvent;
@@ -12,17 +13,23 @@ import javax.servlet.ServletContextListener;
  * A bit of an ugly hack to reset the IdentityManager with a delegating one.
  */
 public class PosixMapperListener implements ServletContextListener {
+    public static final Logger LOGGER = Logger.getLogger(PosixMapperListener.class);
+
     @Override
     public void contextInitialized(ServletContextEvent servletContextEvent) {
+        LOGGER.debug("contextInitialized: START");
         DelegatingAPIKeyIdentityManager.DELEGATED_IDENTITY_MANAGER = AuthenticationUtil.getIdentityManager();
         System.setProperty(IdentityManager.class.getName(), DelegatingAPIKeyIdentityManager.class.getName());
+        LOGGER.debug("contextInitialized: OK");
     }
 
     @Override
     public void contextDestroyed(ServletContextEvent servletContextEvent) {
+        LOGGER.debug("contextDestroyed: START");
         // Reset.
         System.setProperty(IdentityManager.class.getName(),
                            DelegatingAPIKeyIdentityManager.DELEGATED_IDENTITY_MANAGER.getClass().getName());
         DelegatingAPIKeyIdentityManager.DELEGATED_IDENTITY_MANAGER = null;
+        LOGGER.debug("contextDestroyed: OK");
     }
 }
